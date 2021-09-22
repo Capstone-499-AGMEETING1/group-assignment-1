92c1671381a7dd3bf8041c85cb2c98ed09740936
ryankeilty <59891691+ryankeilty@users.noreply.github.com>
2021-09-21 19:11:05 -0700

    Calculate Median (#4)

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 # Function to return the mean of a list of numbers.
 def calculate_mean(numbers):
     return sum(numbers) / len(numbers)
+
+
+def calculate_median(numbers):
+    numbers = sorted(numbers)
+    if len(numbers) % 2 != 0:
+        median = numbers[round(len(numbers) / 2)]
+    else:
+        median = ((numbers[int((len(numbers) / 2)-1)] + numbers[int(len(numbers)/2)]) / 2)
+    return median
