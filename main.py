20762948070250ed393536aa0fbb9746f00fe064
ryankeilty <rkeilty19@gmail.com>
2021-09-21 17:31:15 -0700

    Added Median Calculations and Test

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 # Function to return the mean of a list of numbers.
 def calculate_mean(numbers):
     return sum(numbers) / len(numbers)
+
+
+def calculate_median(numbers):
+    numbers.sort()
+    if len(numbers) % 2 != 0:
+        median = numbers[round(len(numbers) / 2)]
+    else:
+        median = ((len(numbers) / 2) + (len(numbers) / 2 + 1)) / 2
+    return median
--- test_functions.py
+++ test_functions.py
@@ -1,6 +1,11 @@
-from main import calculate_mean
+from main import calculate_mean, calculate_median
 
 
 def test_calculate_mean():
     assert calculate_mean([1, 2, 3, 4, 5, 6, 7, 8]) == 4.5
     assert calculate_mean([10, 36, 2, 3, 78]) == 25.8
+
+
+def test_calculate_median():
+    assert calculate_median([1, 2, 3, 4, 5]) == 3
+    assert calculate_median({4, 2, 7, 2, 6, 2}) == 3
