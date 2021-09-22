b571bfed18883e24100873ea91923b3ad5aaeec7
Chetrrr <61954027+Chetrrr@users.noreply.github.com>
2021-09-22 11:45:10 -0700

    Added Mode Function
    Added mode desc, mode function, mode test

--- test_functions.py
+++ test_functions.py
@@ -1,11 +1,14 @@
-from main import calculate_mean, calculate_median
+from main import calculate_mean, calculate_median, calculate_mode
 
-
-def test_calculate_mean():
+def test_calculate_mean(self):
     assert calculate_mean([1, 2, 3, 4, 5, 6, 7, 8]) == 4.5
     assert calculate_mean([10, 36, 2, 3, 78]) == 25.8
 
 
-def test_calculate_median():
+def test_calculate_median(self):
     assert calculate_median([1, 2, 3, 4, 5]) == 3
     assert calculate_median({4, 3, 7, 2, 6, 5}) == 4.5
+
+def test_calculate_mode(self):
+    assert calculate_mode([1, 2, 3, 4, 4, 5]) == [4]
+    assert calculate_mode([1,2,2,3,3,4,5]) == [2,3]
