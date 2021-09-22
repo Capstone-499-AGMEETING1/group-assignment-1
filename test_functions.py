a67fae03ea3e123dbae56eeb9649b0a98ab0ce5a
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2021-09-22 12:58:12 -0700

    Fix Tests

--- test_functions.py
+++ test_functions.py
@@ -1,14 +1,16 @@
 from main import calculate_mean, calculate_median, calculate_mode
 
-def test_calculate_mean(self):
+
+def test_calculate_mean():
     assert calculate_mean([1, 2, 3, 4, 5, 6, 7, 8]) == 4.5
     assert calculate_mean([10, 36, 2, 3, 78]) == 25.8
 
 
-def test_calculate_median(self):
+def test_calculate_median():
     assert calculate_median([1, 2, 3, 4, 5]) == 3
     assert calculate_median({4, 3, 7, 2, 6, 5}) == 4.5
 
-def test_calculate_mode(self):
+
+def test_calculate_mode():
     assert calculate_mode([1, 2, 3, 4, 4, 5]) == [4]
-    assert calculate_mode([1,2,2,3,3,4,5]) == [2,3]
+    assert calculate_mode([1, 2, 2, 3, 3, 4, 5]) == [2, 3]
